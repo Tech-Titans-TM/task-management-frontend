1e8d06bdd2562fd120b694c3157f23ef6d4ee15b
Shafee Muhsin <shafeem@fortude.co>
2025-04-14 14:47:23 +0530

    cicd test

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./contexts/authContext";
+import ProtectedRoute from "./utils/protectedRoute";
 
-import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { AuthProvider } from './contexts/authContext';
-import ProtectedRoute from './utils/protectedRoute';
-
-import LoginPage from './pages/auth/login';
-import SignupPage from './pages/auth/signup';
-import Home from './pages/home/home';
-import Settings from './pages/settings/settings';
-import AddEditTasksPage from './pages/task-management/add-edit-task-page';
+import LoginPage from "./pages/auth/login";
+import SignupPage from "./pages/auth/signup";
+import Home from "./pages/home/home";
+import Settings from "./pages/settings/settings";
+import AddEditTasksPage from "./pages/task-management/add-edit-task-page";
 
 export default function App() {
   return (
@@ -32,3 +31,5 @@ export default function App() {
     </AuthProvider>
   );
 }
+
+// test
